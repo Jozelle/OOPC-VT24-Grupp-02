e46593fcbb1fcc0774a43046f60b06c3b6d37c24
carolinebarso <70707494+CarolineBarso@users.noreply.github.com>
2024-04-11 13:29:16 +0200

    ändrat lite customer

--- Entities/Customer.cs
+++ Entities/Customer.cs
@@ -9,11 +9,10 @@ namespace CarService.Entities
 {
     internal class Customer
     {
-       
 
-        public int CustomerID {  get; set; }
-        public string FirstName {  get; set; }
-        public  string LastName { get; set; }
+        public int CustomerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
@@ -28,8 +27,16 @@ namespace CarService.Entities
 
         }
 
-        public void DisplayInfo() 
-        { 
+        public string  GetCustomer()
+        {
+            return $"Customer ID={CustomerID},First name={FirstName}, Last name={LastName}, Phone number ={PhoneNumber},";
+
+        }
+
+        public void UpdateCustomer() 
+        
+        {
+        
         }
 
     }
