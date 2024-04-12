49b3fb991b811b4f3d040266238b311a36d1468b
carolinebarso <70707494+CarolineBarso@users.noreply.github.com>
2024-04-12 14:48:55 +0200

    skapat en ny entitet repair item

---
+++ Entities/RepairItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarService.Entities
+{
+    internal class RepairItem
+    {
+
+        public Repair Repair {  get; set; }
+
+        public Item Item { get; set; }
+
+        public int Quantity { get; set;}
+
+    }
+}
