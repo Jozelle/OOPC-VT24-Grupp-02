d70ef36ebb072e5fe799aa76bbfc948513af53f0
carolinebarso <70707494+CarolineBarso@users.noreply.github.com>
2024-04-12 15:07:57 +0200

    busat i RepairItem

--- Entities/Repair.cs
+++ Entities/Repair.cs
@@ -8,11 +8,11 @@ namespace CarService.Entities
 {
     public class Repair
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Description { get; set; }
 
         public Appointment Appointment { get; set; }
-        public ICollection<Item>? Items { get; set; }       
+        public ICollection<RepairItem>? RepairItems { get; set; }       
         public Mechanic RepairedBy { get; set; }
     }
 }
