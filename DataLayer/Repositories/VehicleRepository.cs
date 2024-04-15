89d6b02bf9bf47e8aeab9533a63ded671933e94c
Jozelle <johannagull92@gmail.com>
2024-04-15 08:34:14 +0200

    Test

--- DataLayer/Repositories/VehicleRepository.cs
+++ DataLayer/Repositories/VehicleRepository.cs
@@ -24,11 +24,11 @@ namespace CarService.DataLayer.Repositories
 
         public IEnumerable<Repair> GetJournal(Vehicle vehicle)
         {
-            //var query = from besök in CarServiceContext.Appointments
-            //            join reparation in CarServiceContext.Repairs
-            //            on besök equals reparation.Appointment
-            //            where (besök.Vehicle == vehicle)
-            //            select reparation;
+            var query = from besök in Context.Set<Appointment>()
+                        join reparation in Context.Set<Repair>()
+                        on besök equals reparation.Appointment
+                        where (besök.Vehicle == vehicle)
+                        select reparation;
 
 
             //select new { reparation };
@@ -37,39 +37,39 @@ namespace CarService.DataLayer.Repositories
             // och sen alla repairs kopplade till de besöken.
 
             //??? 
-            IEnumerable<Repair> repairs = CarServiceContext.Repairs
-                .Join(
-                CarServiceContext.Appointments,
-                r => r.Appointment.AppointmentId,
-                b => b.AppointmentId,
-                (r, b) => new
-                {
-                    RepairId = r.RepairId,
-                    Description = r.Description,
-                    Appointment = r.Appointment,
-                    RepairItems = r.RepairItems,
-                    RepairedBy = r.RepairedBy,
-                    Vehicle = b.Vehicle
-                })
-                .Where(x => x.Vehicle == vehicle)
-                .Select(x => new Repair()
-                {
-                    RepairId = x.RepairId,
-                    Description = x.Description,
-                    Appointment = x.Appointment,
-                    RepairItems = x.RepairItems,
-                    RepairedBy = x.RepairedBy,
-                });
+            //IEnumerable<Repair> repairs = CarServiceContext.Repairs
+            //    .Join(
+            //    CarServiceContext.Appointments,
+            //    r => r.Appointment.AppointmentId,
+            //    b => b.AppointmentId,
+            //    (r, b) => new
+            //    {
+            //        RepairId = r.RepairId,
+            //        Description = r.Description,
+            //        Appointment = r.Appointment,
+            //        RepairItems = r.RepairItems,
+            //        RepairedBy = r.RepairedBy,
+            //        Vehicle = b.Vehicle
+            //    })
+            //    .Where(x => x.Vehicle == vehicle)
+            //    .Select(x => new Repair()
+            //    {
+            //        RepairId = x.RepairId,
+            //        Description = x.Description,
+            //        Appointment = x.Appointment,
+            //        RepairItems = x.RepairItems,
+            //        RepairedBy = x.RepairedBy,
+            //    });
 
 
-            return repairs;
+            return query;
         }
 
 
 
-        public CarServiceContext CarServiceContext
-        {
-            get { return CarServiceContext as CarServiceContext; }
-        }
+        //public CarServiceContext CarServiceContext
+        //{
+        //    get { return CarServiceContext as CarServiceContext; }
+        //}
     }
 }
