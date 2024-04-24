5bb1fddc180f1fa91a709075d66faad33a234cc1
carolinebarso <70707494+CarolineBarso@users.noreply.github.com>
2024-04-24 10:13:57 +0200

    Utökat vyer i WPF, skapat metoder i Controller, ändrat och lagt till i repos och Uow samt ändrat i entiteter

--- CarService.PresentationLayer.WPF/Main.xaml.cs
+++ CarService.PresentationLayer.WPF/Main.xaml.cs
@@ -23,5 +23,65 @@ namespace CarService.PresentationLayer.WPF
         {
             InitializeComponent();
         }
+
+        private void btn_CreateBooking_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_SearchAppointment_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_SearchRegNo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_SubmitVehicle_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DatePicker_CalendarOpened(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rbtn_CardPayment_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rbtn_CashPayment_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rbtn_SwishPayment_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rbtn_CardPayment_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rbtn_CashPayment_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rbtn_SwishPayment_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_CheckoutPayment_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
