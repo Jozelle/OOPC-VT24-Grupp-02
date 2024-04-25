6ac51b1d49a5c72e62865c06b5e2b584b2cc0e24
carolinebarso <70707494+CarolineBarso@users.noreply.github.com>
2024-04-25 16:52:26 +0200

    Lagt till väsentliga metoder

--- BusinessLayer/CarServiceController.cs
+++ BusinessLayer/CarServiceController.cs
@@ -81,5 +81,80 @@ namespace BusinessLayer
                 uow.Complete();
             }
         }
+
+        public Vehicle CreateVehicle(Vehicle vehicle)
+        {
+            using (UnitOfWork uow = new UnitOfWork())
+            {
+                uow.Vehicles.Add(vehicle);
+                uow.Complete();
+
+            }
+            return vehicle;
+
+        }
+
+        public void SaveVehicle(Vehicle vehicle)
+        {
+            using (UnitOfWork uow = new UnitOfWork())
+            {
+                uow.Complete();
+            }
+        }
+
+        public void CreateCustomer(Customer customer)
+        {
+            using (UnitOfWork uow = new UnitOfWork())
+            {
+                uow.Customers.Add(customer);
+                uow.Complete();
+
+            }
+        }
+
+        public void SaveCustomer(Customer customer)
+        {
+            using (UnitOfWork unitOfWork = new UnitOfWork())
+            {
+                unitOfWork.Complete();
+            }
+
+        }
+
+        public Appointment CreateAppointment(Appointment appointment)
+        {
+            using (UnitOfWork uow = new UnitOfWork())
+            {
+
+                uow.Appointments.Add(appointment);
+                uow.Complete();
+                return appointment;
+            }
+        }
+
+        public Customer GetCustomerBySSN(string id)
+        {
+            using (UnitOfWork uow = new UnitOfWork())
+            {
+                return uow.Customers.GetBySocialSecurityNo(id);
+            }
+        }
+
+        public Customer GetCustomerByPhone(string number)
+        {
+            using (UnitOfWork uow = new UnitOfWork())
+            {
+                return uow.Customers.GetByPhoneNo(number);
+            }
+        }
+
+        public Customer GetCustomerByFullName(string fName, string lName)
+        {
+            using (UnitOfWork uow = new UnitOfWork())
+            {
+                return uow.Customers.GetByFullName(fName, lName);
+            }
+        }
+
     }
 }
