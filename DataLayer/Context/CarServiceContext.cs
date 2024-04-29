3ead40fb2173c1465791d733e753e876036ee86b
Jozelle <51460429+Jozelle@users.noreply.github.com>
2024-04-29 11:03:24 +0200

    Uppdaterar Entities

--- DataLayer/Context/CarServiceContext.cs
+++ DataLayer/Context/CarServiceContext.cs
@@ -20,9 +20,9 @@ namespace CarService.DataLayer.Context
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=CarService;Integrated Security=True");
+            //optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=CarService;Integrated Security=True");
 
-            //optionsBuilder.UseSqlServer(@"Server=ROGER\SQLEXPRESS;Database=test2;Trusted_Connection=true;TrustServerCertificate=true");
+            optionsBuilder.UseSqlServer(@"Server=ROGER\SQLEXPRESS;Database=test3;Trusted_Connection=true;TrustServerCertificate=true");
 
             //optionsBuilder.UseSqlServer(@"Server=REMOVED-HOST, 56077;Database=REMOVED-IDENTIFIER;User=REMOVED-IDENTIFIER;Password=REMOVED-PASSWORD;TrustServerCertificate=true");
             base.OnConfiguring(optionsBuilder);
