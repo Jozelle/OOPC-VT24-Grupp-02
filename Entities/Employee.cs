949ff4b0d4355a5b09b7cfc4fca6132157688318
Jozelle <johannagull92@gmail.com>
2024-05-03 12:10:20 +0200

    Lirar inte med abstrakta metaklasser, tagit bort så får vi felsöka mer framöver

--- Entities/Employee.cs
+++ Entities/Employee.cs
@@ -2,7 +2,7 @@
 
 namespace CarService.Entities
 {
-    public abstract class Employee
+    public class Employee
     {
         public int EmployeeId { get; set; }
         public string FirstName { get; set; }
