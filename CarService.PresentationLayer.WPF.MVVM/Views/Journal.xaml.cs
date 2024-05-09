8fdf02ada2049239037d6db380668074522bea32
Jozelle <51460429+Jozelle@users.noreply.github.com>
2024-05-09 15:31:36 +0200

    Första vyn till MVVM, woh!

--- CarService.PresentationLayer.WPF.MVVM/Views/Journal.xaml.cs
+++ CarService.PresentationLayer.WPF.MVVM/Views/Journal.xaml.cs
@@ -14,43 +14,6 @@ namespace CarService.PresentationLayer.WPF.MVVM.Views
         public Journal(/*string regNo*/)
         {
             InitializeComponent();
-
-            string regNo = "abc123";
-
-            Vehicle vehicle = vehicleController.GetVehicle(regNo);
-
-            labelRegistrationNo.Content = vehicle.RegistrationNumber;
-            labelMake.Content = vehicle.Make;
-            labelModel.Content = vehicle.Model;
-            labelYear.Content = vehicle.Year;
-
-            List<string> journalRows = new List<string>();
-            List<Appointment> appointmentsInJournal = vehicleController.GetJournal(vehicle.RegistrationNumber);
-
-            foreach (Appointment appointment in appointmentsInJournal)
-            {
-                journalRows.Add(appointment.SubmissionDate.ToShortDateString() + " - " + appointment.Purpose);
-                foreach (UsedItem ui in appointment.UsedItems)
-                {
-                    journalRows.Add($"Item: {ui.Item.Description}\t{ui.Quantity} pcs");
-                }
-                foreach (Comment comment in appointment.Comments)
-                {
-                    journalRows.Add($"Added by: {comment.Author.FirstName}\t{comment.Message}");
-                }
-            }
-
-            lbxJournal.ItemsSource = journalRows;
-        }
-
-        private void btnSend_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("This method is not implemented yet, sorry!");
-        }
-
-        private void btnExit_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
         }
     }
 }
