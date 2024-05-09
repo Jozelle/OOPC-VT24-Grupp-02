74db24da2e60712626c3b25ac2d3852620ce1ec3
Jozelle <johannagull92@gmail.com>
2024-05-09 23:10:52 +0200

    Jobbat med att översätta mekanikervyn till MVVM

--- CarService.PresentationLayer.WPF.MVVM/Views/Journal.xaml.cs
+++ CarService.PresentationLayer.WPF.MVVM/Views/Journal.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using CarService.BusinessLayer;
 using CarService.Entities;
+using CarService.PresentationLayer.WPF.MVVM.Services;
+using CarService.PresentationLayer.WPF.MVVM.ViewModels;
 using System.Windows;
 using MessageBox = System.Windows.MessageBox;
 
@@ -8,12 +10,13 @@ namespace CarService.PresentationLayer.WPF.MVVM.Views
     /// <summary>
     /// Interaction logic for Journal.xaml
     /// </summary>
-    public partial class Journal : Window
+    public partial class Journal : Window, ICloseable
     {
-        VehicleController vehicleController = new();
-        public Journal(/*string regNo*/)
+
+        public Journal()
         {
             InitializeComponent();
+
         }
     }
 }
