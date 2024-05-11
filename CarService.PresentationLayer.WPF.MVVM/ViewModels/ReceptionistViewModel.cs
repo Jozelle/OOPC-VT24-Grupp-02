1b5940cffa9bf0c033c1f9420374a9db6aa79433
carolinebarso <70707494+CarolineBarso@users.noreply.github.com>
2024-05-11 15:04:21 +0200

    MVVM receptionist typ klar

---
+++ CarService.PresentationLayer.WPF.MVVM/ViewModels/ReceptionistViewModel.cs
@@ -0,0 +1,298 @@
+﻿using CarService.BusinessLayer;
+using CarService.Entities;
+using CarService.PresentationLayer.WPF.MVVM.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CarService.PresentationLayer.WPF.MVVM.Models;
+using CarService.Entities.Enums;
+using CarService.PresentationLayer.WPF.MVVM.Services;
+using System.Windows;
+using Microsoft.EntityFrameworkCore.Update.Internal;
+
+namespace CarService.PresentationLayer.WPF.MVVM.ViewModels
+{
+    public class ReceptionistViewModel : ObservableObject
+    {
+
+        //Controllers
+        private AppointmentController appointmentController;
+        private VehicleController vehicleController;
+        private EmployeeController employeeController;
+
+
+        private IWindowService windowService;
+
+        private int loggedInId;
+
+
+
+
+        //Properties
+
+        private string searchAppointment = "Type here..";
+        public string SearchAppointment
+        {
+            get { return searchAppointment; }
+            set { searchAppointment = value; }
+        }
+
+        private int searchType = 0;
+        public int SearchType
+        {
+            get { return searchType; }
+            set { searchType = value; }
+        }
+
+        private ObservableCollection<Appointment> appointments = null!;
+        public ObservableCollection<Appointment> Appointments
+        {
+            get { return appointments; }
+            set
+            {
+                appointments = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Appointment selectedAppointment = null!;
+        public Appointment SelectedAppointment
+        {
+            get { return selectedAppointment; }
+            set
+            {
+                selectedAppointment = value;
+                OnPropertyChanged();
+                if (selectedAppointment != null)
+                {
+                    CurrentAppointment = selectedAppointment;
+                }
+            }
+        }
+
+
+
+        private List<AppointmentStatus> appointmentsStatuses = null!;
+        public List<AppointmentStatus> AppointmentsStatuses
+        {
+            get { return appointmentsStatuses; }
+            set
+            {
+                appointmentsStatuses = value;
+                OnPropertyChanged();
+            }
+        }
+
+      
+
+        private AppointmentStatus selectedAppointmentStatus = AppointmentStatus.CarReceived;
+        public AppointmentStatus SelectedAppointmentStatus
+        {
+            get { return selectedAppointmentStatus; }
+            set
+            {
+                selectedAppointmentStatus = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Appointment currentAppointment = null!;
+        public Appointment CurrentAppointment
+        {
+            get { return currentAppointment; }
+            set
+            {
+                currentAppointment = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string inputComment = null!;
+        public string InputComment
+        {
+            get { return inputComment; }
+            set
+            {
+                inputComment = value;
+                OnPropertyChanged();
+            }
+        }
+
+     
+
+        private ObservableCollection<Appointment> todaysAppointments = null!;
+        public ObservableCollection<Appointment> TodaysAppointments
+        {
+            get { return todaysAppointments; }
+            set
+            {
+                todaysAppointments = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Vehicle currentVehicle = null!; 
+        public Vehicle CurrentVehicle
+        {
+            get { return currentVehicle; }
+            set
+            {
+                currentVehicle = value;
+                OnPropertyChanged();
+            }
+        }
+
+       
+
+      
+
+
+
+
+        //Constructor
+
+        public ReceptionistViewModel(int id)
+        {
+
+            appointmentController = new AppointmentController();
+            vehicleController = new VehicleController();
+            employeeController = new EmployeeController();
+
+            Appointments = new ObservableCollection<Appointment>(appointmentController.GetTodaysAppointments());
+
+            windowService = new WindowService();
+
+            loggedInId = id;
+            AppointmentsStatuses = Enum.GetValues(typeof(AppointmentStatus)).Cast<AppointmentStatus>().ToList();
+
+            TodaysAppointments = new ObservableCollection<Appointment>();
+
+            RefreshCommand.Execute(null);
+        }
+
+
+
+
+
+        //Commands
+
+        private ICommand refreshCommand = null!;
+        public ICommand RefreshCommand => refreshCommand ??= refreshCommand = new RelayCommand(() =>
+        {
+            Appointments = new ObservableCollection<Appointment>(appointmentController.GetTodaysAppointments());
+
+            //IsNotModified = true;
+        });
+
+
+        private ICommand exitCommand = null!;
+        public ICommand ExitCommand => exitCommand ??= exitCommand = new RelayCommand(() => App.Current.Shutdown());
+
+
+        private ICommand searchAppointmentCommand = null!;
+        public ICommand SearchAppointmentCommand => searchAppointmentCommand ??= searchAppointmentCommand = new RelayCommand(() =>
+        {
+            if (SearchType == 0)
+            {
+                //SSNo.
+                Appointments = new ObservableCollection<Appointment>(appointmentController.GetAppointmentsBySSNo(SearchAppointment));
+
+            }
+
+            else if (SearchType == 1)
+            {
+                //PhoneNo.
+                Appointments = new ObservableCollection<Appointment>(appointmentController.GetAppointmentsByPhoneNo(SearchAppointment));
+            }
+
+            else if (SearchType == 2)
+            {
+                //FullName
+                string[] nameStrings = SearchAppointment.Split(",");
+                if (nameStrings != null && nameStrings.Length == 2)
+                {
+                    Appointments = new ObservableCollection<Appointment>(appointmentController.GetAppointmentsByFullName(nameStrings[0], nameStrings[1]));
+                }
+
+            }
+
+            else if (SearchType == 3)
+            {
+                //RegNo.
+                Appointments = new ObservableCollection<Appointment>(appointmentController.GetAppointmentsByRegNo(SearchAppointment));
+
+            }
+        });
+
+        private ICommand saveStatusCommand = null!;
+        public ICommand SaveStatusCommand => saveStatusCommand ??= saveStatusCommand = new RelayCommand(() =>
+        {
+            if (SelectedAppointment != null)
+            {
+                SelectedAppointment.Status = SelectedAppointmentStatus;
+                appointmentController.SaveChanges(SelectedAppointment);
+
+                RefreshCommand.Execute(null);
+                MessageBox.Show("Status updated successfully!");
+
+                
+            }
+        });
+
+
+        private ICommand addCommentCommand = null!;
+        public ICommand AddCommentCommand => addCommentCommand ??= addCommentCommand = new RelayCommand(() =>
+        {
+            if (InputComment != null)
+            {
+                Employee employee = employeeController.GetEmployee(loggedInId);
+                int affectedrows = appointmentController.AddCommentToAppointment(CurrentAppointment, InputComment, employee);
+
+                Comment comment = new Comment()
+                {
+                    AuthorEmployeeId = loggedInId,
+                    Time = DateTime.Now,
+                    Message = InputComment,
+                    Appointment = CurrentAppointment
+                   
+                };
+
+                // Add the comment to the collection of comments for the selected appointment
+                if (CurrentAppointment.Comments == null)
+                {
+                    CurrentAppointment.Comments = new List<Comment>();
+                }
+
+                CurrentAppointment.Comments.Add(comment);
+
+                MessageBox.Show(affectedrows.ToString());
+
+
+            }
+        });
+
+
+        //private ICommand viewJournalCommand = null!;
+        //public ICommand ViewJournalCommand => viewJournalCommand ??= viewJournalCommand = new RelayCommand(() =>
+        //{
+        //    JournalViewModel journalViewModel = new JournalViewModel(CurrentVehicle.RegistrationNumber);
+        //    bool result = windowService.ShowDialog(journalViewModel);
+        //});
+
+
+
+
+    }
+
+
+}
+
+
+    
+
+
---
+++ CarService.PresentationLayer.WPF.MVVM/Views/ReceptionistView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CarService.PresentationLayer.WPF.MVVM.Services;
+using CarService.PresentationLayer.WPF.MVVM.ViewModels;
+
+namespace CarService.PresentationLayer.WPF.MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for ReceptionistView.xaml
+    /// </summary>
+    public partial class ReceptionistView : Window
+    {
+        public ReceptionistView()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
