e9f08fe91c939ece79eaaa21a6786a35e86277b9
carolinebarso <70707494+CarolineBarso@users.noreply.github.com>
2024-05-13 11:53:22 +0200

    gjort ändringar i vyn och dess koppling

--- CarService.PresentationLayer.WPF.MVVM/ViewModels/CreateVehicleViewModel.cs
+++ CarService.PresentationLayer.WPF.MVVM/ViewModels/CreateVehicleViewModel.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace CarService.PresentationLayer.WPF.MVVM.ViewModels
@@ -20,6 +21,65 @@ namespace CarService.PresentationLayer.WPF.MVVM.ViewModels
 
 
         //Properties
+
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        private string socialSecurityNumber;
+        public string SocialSecurityNumber
+        {
+            get { return socialSecurityNumber; }
+            set { socialSecurityNumber = value; }
+        }
+
+       private string streetName;   
+        public string StreetName
+        {
+            get { return streetName; }
+            set { streetName = value; }
+        }
+
+        private string postalCode;
+        public string PostalCode
+        {
+            get { return postalCode; }
+            set { postalCode = value; }
+        }
+
+        private string city;    
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        private string phoneNumber;
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+
+
         private Customer customer = null!;
         public Customer Customer
         {
@@ -48,6 +108,14 @@ namespace CarService.PresentationLayer.WPF.MVVM.ViewModels
             set { registrationNumber = value; }
         }
 
+        public CreateVehicleViewModel()
+        {
+            customerController = new CustomerController();
+            vehicleController = new VehicleController();
+
+           
+        }
+
 
         //Commands
 
@@ -55,6 +123,43 @@ namespace CarService.PresentationLayer.WPF.MVVM.ViewModels
         public ICommand AddCustomerCommand => addCustomerCommand ??= addCustomerCommand = new RelayCommand(() =>
         {
 
+            Customer addCustomer = new Customer(FirstName, LastName, PhoneNumber, SocialSecurityNumber, StreetName, PostalCode, City, Email);
+            if (addCustomer.Email == "")
+            {
+                addCustomer.Email = null;
+            }
+
+            if (addCustomer.Address == "")
+            {
+                addCustomer.Address = null;
+            }
+
+            if (addCustomer.PostalCode == "")
+            {
+                addCustomer.PostalCode = null;
+            }
+
+            if (addCustomer.City == "")
+            {
+                addCustomer.City = null;
+            }
+            
+            int rowsAffected = customerController.SaveCustomer(addCustomer);
+            if (rowsAffected > 0)
+            {
+                MessageBox.Show("Customer was added!");
+            }
+
+            else if (rowsAffected == -1)
+            {
+                MessageBox.Show("Customer already exists!");
+            }
+
+            else
+            {
+                MessageBox.Show("Could not add customer!");
+            }   
+                
         });
 
         private ICommand addVehicleCommand = null!;
